b4dabfb13a6a20e350abd906291ae4921045c8fa
Birgul Yuksel <birgulalpyuksel@gmail.com>
2023-10-22 15:06:24 +0300

    Pratik4

---
+++ Java101/src/Pratik4.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Pratik4 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Kombinasyon için n giriniz: ");
+		int n = input.nextInt();
+		System.out.println("Kombinasyon için r giriniz: ");
+		int r = input.nextInt();
+		int nTotal=1;
+		int rTotal=1;
+		int nrtotal=1;
+		
+		for(int i=1; i<= n ; i++) {
+			nTotal = nTotal * i;
+			
+		}
+		System.out.println("N Faktöriyel: " + nTotal);
+		
+		for(int i=1; i<= r ; i++) {
+			rTotal = rTotal * i;
+			
+		}
+		System.out.println("R Faktöriyel: " + rTotal);
+		
+		for(int i=1; i<= (n-r) ; i++) {
+			nrtotal = nrtotal * i;
+			
+		}
+		System.out.println("n-r Faktöriyel: " + nrtotal);
+		
+		System.out.println("C(n,r) =  " + (nTotal/(rTotal * nrtotal)));
+			
+		
+
+	}
+
+}
